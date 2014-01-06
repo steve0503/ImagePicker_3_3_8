306cc37d5f9b5694c0376f1a0c4c7d60a5f673aa
steve0503 <leehr76@gmail.com>
2014-01-06 14:58:17 +0900

    ImagePickerTest
    Done

--- ImagePicker_3_3_8/ViewController.m
+++ ImagePicker_3_3_8/ViewController.m
@@ -8,11 +8,61 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#define MAX_NUM 5
+
+@interface ViewController ()<UIPickerViewDelegate, UIPickerViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UIPickerView *picker;
 
 @end
 
 @implementation ViewController
+- (IBAction)selectRandom:(id)sender {
+    int r = arc4random()% MAX_NUM;
+    [self.picker selectRow:r inComponent:0 animated:YES];
+    
+}
+
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    
+    return 1;
+    
+}
+
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    
+    return MAX_NUM;
+    
+}
+
+
+-(CGFloat)pickerView:(UIPickerView *)pickerView rowHeightForComponent:(NSInteger)component
+{
+    return 64;
+}
+
+-(UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view{
+    
+    NSString *imagePath = [NSString stringWithFormat:@"iu%ld.jpg",row];
+    UIImage *image = [UIImage imageNamed:imagePath];
+    UIImageView * imageView;
+    
+    if (nil == view) {
+        imageView = [[UIImageView alloc] initWithImage:image];
+        imageView.frame = CGRectMake(0,0,100,60);
+        
+        
+    }
+    else{
+        imageView = (UIImageView *)view;
+        imageView.image = image;
+        
+        
+    }
+    
+    return imageView;
+    
+}
 
 - (void)viewDidLoad
 {
